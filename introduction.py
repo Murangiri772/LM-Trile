db991a4bab8f80ad9433366ad5d21a1022754eed
Murangiri772 <murangiri772@gmail.com>
2025-02-04 14:32:07 +0300

    first commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display a value
+
+print("Lewis")
+print("eMobilis")
+print(700)
+print(10+20)
---
+++ variable.py
@@ -0,0 +1,33 @@
+#variable is a container used to store a value
+
+location ="westlands"
+firstname ="Lewis"
+lastname ="Ruto"
+school ="eMobilis"
+x =36
+y =11
+
+
+l =39
+m =2
+z =78
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+
+
+
+print(firstname,lastname)
+
+
+print("My first name is",firstname,lastname)
+print("My school name is",school)
+print(x + y )
+
+print(l * m)
+print("The product of",l,"and",m,"is",z)
+
+
+
